a11ac6b272ad0b4359b339b6d3d966ee2c032228
shreevansh23 <shreevansh.n@moksa.ai>
2025-12-29 14:22:28 +0530

    update the vite file

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: "/Zordr-Landing-Page/",
+  base: "/", // ✅ THIS IS THE FIX FOR VERCEL
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
